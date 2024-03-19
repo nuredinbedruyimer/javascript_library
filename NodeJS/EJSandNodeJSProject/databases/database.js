7eb72e8179c573186a369eccccba5c1d925fd272
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-03-19 12:00:00 +0300

    Uploading Image Using Multer and User Creation Handler Done

--- NodeJS/EJSandNodeJSProject/databases/database.js
+++ NodeJS/EJSandNodeJSProject/databases/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017";
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/EJS";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(MONGO_URL);
--- NodeJS/EJSandNodeJSProject/routes/userRoutes.js
+++ NodeJS/EJSandNodeJSProject/routes/userRoutes.js
@@ -1,6 +1,49 @@
 import express from "express";
+import multer from "multer";
+import { User } from "../models/userModle.js";
 
 const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
+}).single("file");
+
+router.post("/adduser", upload, async (req, res) => {
+  try {
+    const { name, email, phone } = req.body;
+
+    // console.log(req.file);
+    const user = new User({
+      name,
+      email,
+      phone,
+      image: req.file.filename,
+    });
+    await user.save();
+
+    req.session.message = {
+      type: "success",
+      message: "User Created Successfully !",
+    };
+    res.redirect("/");
+  } catch (error) {
+    console.log("Error In Image Uploading : ", error);
+    res.status(500).json({
+      success: false,
+      message: "Error In Creating User",
+    });
+  }
+});
 
 router.get("/", (req, res) => {
   res.render("index", { title: "EJS" });

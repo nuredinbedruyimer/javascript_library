5884ed6fe3e91410b42b43ec1dd7793b2a192487
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-03-20 12:00:00 +0300

    Update Controller

--- NodeJS/EJSandNodeJSProject/routes/userRoutes.js
+++ NodeJS/EJSandNodeJSProject/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import { User } from "../models/userModle.js";
+import fs from "fs";
 
 const router = express.Router();
 
@@ -58,6 +59,68 @@ router.get("/", async (req, res) => {
 });
 router.get("/adduser", (req, res) => {
   res.render("add_user", { title: "AddUser" });
+});
+
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const userID = req.params.id;
+    console.log("User : ", userID);
+    const existingUser = await User.findOne({ _id: userID });
+    console.log("User : ", existingUser);
+    if (!existingUser) {
+      res.redirect("/");
+    }
+    res.render("update_user", {
+      title: "Update User",
+      user: existingUser,
+    });
+  } catch (error) {
+    console.log("Error In Edit ROute");
+    res.redirect("/");
+  }
+});
+
+router.post("/edit/:id", upload, async (req, res) => {
+  try {
+    const userID = req.params.id;
+    const { name, email, phone, old_image } = req.body;
+    console.log("Old Image : ", old_image);
+    let new_image = "";
+    if (req.file) {
+      new_image = req.file.filename;
+      try {
+        fs.unlinkSync(`uploads/${req.body.old_image}`);
+      } catch (error) {
+        console.log("Error In file Deletion : ", error);
+        res.status(500).json({
+          message: "File Deletion Error",
+        });
+        return;
+      }
+    } else {
+      new_image = req.body.old_image;
+    }
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: userID },
+      {
+        name,
+        email,
+        phone,
+        image: new_image,
+      }
+    );
+    console.log("Updated User : ", updatedUser);
+    req.session.message = {
+      type: "success",
+      message: "User Updated Successfully",
+    };
+    res.redirect("/");
+  } catch (error) {
+    console.log("Error In User Update : ", error);
+    res.status(500).json({
+      message: "User Updation Failure",
+    });
+  }
 });
 
 export default router;

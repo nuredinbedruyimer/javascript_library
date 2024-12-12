11e3bfa172df82c518f044f493eceb5ee2567c24
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-12-12 12:50:10 +0300

    'Function  In Typescript'

---
+++ typescript/function/build/js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+function sayHello(name) {
+    return `Hello  ${name}`;
+}
+let userName = "Shiv Khera";
+console.log(sayHello(userName));
---
+++ typescript/function/src/main.ts
@@ -0,0 +1,5 @@
+function sayHello(name: string) {
+  return `Hello  ${name}`;
+}
+let userName: string = "Shiv Khera";
+console.log(sayHello(userName));

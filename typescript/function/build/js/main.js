cbb2e74c46c1c3c189cec2ad06b9f61af42a23c8
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-12-12 12:50:10 +0300

    'Function 'Optional and Default Parameter'  In Typescript'

--- typescript/function/build/js/main.js
+++ typescript/function/build/js/main.js
@@ -4,3 +4,52 @@ function sayHello(name) {
 }
 let userName = "Shiv Khera";
 console.log(sayHello(userName));
+function calculateDiscount(price, percentage) {
+    return price * percentage;
+}
+let oldPrice = 1200;
+let percentage = 0.95;
+let newPrice = calculateDiscount(oldPrice, percentage);
+console.log("New Price After Discount : ", newPrice);
+let names = [
+    "CodeCraft",
+    "DevSphere",
+    "ByteHive",
+    "LogicLayer",
+    "CloudForge",
+    "PixelPeak",
+    "StackStride",
+    "SyntaxHub",
+    "DataDock",
+    "AppNucleus",
+];
+function isNameFound(inputName) {
+    for (let name of names) {
+        if (name === inputName) {
+            return true;
+        }
+    }
+    return false;
+}
+let selectedName = [
+    "CloudForge",
+    "PixelPeak",
+    "StackStride",
+    "StackStrided",
+    "SyntaxHub",
+];
+let selectedNames = [];
+for (let name of selectedName) {
+    if (isNameFound(name)) {
+        selectedNames.push(name);
+    }
+}
+console.log("Selected Name : ", selectedNames);
+//  optional Parameter
+function calculateTotalPrice(itemsCount, unitPrice = 1, percentage = 0) {
+    let totalPrice = (unitPrice || 0) * itemsCount;
+    let discount = (percentage || 0) * totalPrice;
+    return totalPrice - discount;
+}
+let totalSpent = calculateTotalPrice(100, 20);
+console.log("Total Price : ", totalSpent);
--- typescript/function/src/main.ts
+++ typescript/function/src/main.ts
@@ -3,3 +3,72 @@ function sayHello(name: string) {
 }
 let userName: string = "Shiv Khera";
 console.log(sayHello(userName));
+
+function calculateDiscount(price: number, percentage: number): number {
+  return price * percentage;
+}
+
+let oldPrice: number = 1200;
+let percentage: number = 0.95;
+
+let newPrice: number = calculateDiscount(oldPrice, percentage);
+
+console.log("New Price After Discount : ", newPrice);
+
+let names: string[] = [
+  "CodeCraft",
+  "DevSphere",
+  "ByteHive",
+  "LogicLayer",
+  "CloudForge",
+  "PixelPeak",
+  "StackStride",
+  "SyntaxHub",
+  "DataDock",
+  "AppNucleus",
+];
+
+function isNameFound(inputName: string): boolean {
+  for (let name of names) {
+    if (name === inputName) {
+      return true;
+    }
+  }
+  return false;
+}
+
+let selectedName: string[] = [
+  "CloudForge",
+  "PixelPeak",
+  "StackStride",
+  "StackStrided",
+
+  "SyntaxHub",
+];
+
+let selectedNames: string[] = [];
+
+for (let name of selectedName) {
+  if (isNameFound(name)) {
+    selectedNames.push(name);
+  }
+}
+
+console.log("Selected Name : ", selectedNames);
+
+//  optional Parameter
+
+function calculateTotalPrice(
+  itemsCount: number,
+  unitPrice: number = 1,
+  percentage: number = 0
+): number {
+  let totalPrice: number = (unitPrice || 0) * itemsCount;
+  let discount: number = (percentage || 0) * totalPrice;
+
+  return totalPrice - discount;
+}
+
+let totalSpent: number = calculateTotalPrice(100, 20);
+
+console.log("Total Price : ", totalSpent);
